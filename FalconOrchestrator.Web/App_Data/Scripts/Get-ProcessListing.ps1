d7ca48c9ff584fdb5dabb72a30fde205371490fb
Evan Burns <evan.burns@crowdstrike.com>
2016-12-12 09:48:48 -0500

    get rid of multi-line issue

--- FalconOrchestrator.Web/App_Data/Scripts/Get-ProcessListing.ps1
+++ FalconOrchestrator.Web/App_Data/Scripts/Get-ProcessListing.ps1
@@ -2,7 +2,4 @@
 $ppid = @{}
 $cmdline = @{}
 Get-WmiObject win32_process |ForEach-Object {$owners[$_.Handle] = $_.GetOwner().User; $ppid[$_.handle] = $_.ParentProcessId; $cmdline[$_.handle] = $_.CommandLine}
-Get-Process | Select-Object Id,  @{n="ParentProcessId";e={$ppid[$_.Id.ToString()]}}, Company,Name,` 
-@{n="Owner";e={$owners[$_.Id.ToString()]}},Path, @{n="CommandLine";e={$cmdline[$_.Id.ToString()]}},ProductVersion,StartTime,`
-CPU,HandleCount,WorkingSet,PagedMemorySize,PrivateMemorySize,VirtualMemorySize,NonpagedSystemMemorySize,PagedSystemMemorySize,`
-PeakPagedMemorySize,PeakWorkingSet,PeakVirtualMemorySize, Description
+Get-Process | Select-Object Id,  @{n="ParentProcessId";e={$ppid[$_.Id.ToString()]}}, Company,Name,@{n="Owner";e={$owners[$_.Id.ToString()]}},Path, @{n="CommandLine";e={$cmdline[$_.Id.ToString()]}},ProductVersion,StartTime,CPU,HandleCount,WorkingSet,PagedMemorySize,PrivateMemorySize,VirtualMemorySize,NonpagedSystemMemorySize,PagedSystemMemorySize,PeakPagedMemorySize,PeakWorkingSet,PeakVirtualMemorySize, Description
